8cc7dfbf92d9c3c0961f2f71b31813c00ff81059
Kevin <mail@kevinchandler.co.uk>
2019-05-11 20:21:59 +0100

    wait for stop and start while creating snapshots

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -83,10 +83,20 @@ def create_snapshots(project):
     instance=filter_instances(project)
 
     for i in instance:
+        
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshot cli")
 
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Job Done")
     return
 
 @instances.command('list')
