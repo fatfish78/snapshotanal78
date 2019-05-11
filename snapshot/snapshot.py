99747bc37e705a5bc0a62a8deea9136402172697
Kevin <mail@kevinchandler.co.uk>
2019-05-11 20:09:59 +0100

    List volumes and snapshots

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -16,9 +16,78 @@ def filter_instances(project):
     return(instance)
 
 @click.group()
+def cli():
+    """snapshot manages snapshots and instances"""
+
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project',default=None,
+    help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+    "List Snapshots"
+
+    instance=filter_instances(project)
+
+    for i in instance:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(', '.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    return
+
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project',default=None,
+    help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List Volumes"
+
+    instance=filter_instances(project)
+
+    for i in instance:
+        for v in i.volumes.all():
+            print(', '.join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
 
+@instances.command('snapshot')
+@click.option('--project',default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Create snapshot for EC2 Instances"
+    instance=filter_instances(project)
+
+    for i in instance:
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by snapshot cli")
+
+    return
 
 @instances.command('list')
 @click.option('--project',default=None,
@@ -30,11 +99,11 @@ def list_instances(project):
     for i in instance:
         tags = { t['Key']:t['Value'] for t in i.tags or [] }
         print(', '.join((
-        i.id,
-        i.instance_type,
-        i.state['Name'],
-        i.public_dns_name,
-        tags.get('project','<no project>')
+            i.id,
+            i.instance_type,
+            i.state['Name'],
+            i.public_dns_name,
+            tags.get('project','<no project>')
         )))
     return
 
@@ -61,4 +130,4 @@ def stop_instances(project):
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
